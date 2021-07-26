ae121fb98ca830413b2c7eb609e9650891fa91c3
Alex Carrega <contact@alexcarrega.com>
2021-07-26 02:54:16 +0200

    Version 3 update

--- version1/deployment.py
+++ version1/deployment.py
@@ -1,45 +1,48 @@
-from kafka import KafkaConsumer, KafkaProducer
-from joblib import dump, load
+import ast
+import json
+import time
+from os import environ
+
 import pandas as pd
-import numpy as np
-from os import listdir
-import sys, re, csv, ast, json, time
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
+from joblib import load
+from kafka import KafkaConsumer, KafkaProducer
 
 scaler = load('scaler.joblib')
 cols = load('columns.joblib')
-grid_clf_acc= load('rfmodel.joblib')
+grid_clf_acc = load('rfmodel.joblib')
 
-###edit correct addressess here
-producer = KafkaProducer(bootstrap_servers='10.0.0.7:29092')
-consumer = KafkaConsumer('network-data', bootstrap_servers='10.0.0.7:29092')
-counter=0
+kafka_bootstrap_servers = environ.get(
+    'KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
+kafka_topic = environ.get('KAFKA_TOPIC', 'network-data')
+
+producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+consumer = KafkaConsumer(
+    kafka_topic, bootstrap_servers=kafka_bootstrap_servers)
+
+counter = 0
 for msg in consumer:
-    
-    message = msg.value.decode('utf-8')
-    message2=ast.literal_eval(message)
 
-    testing=[]
+    message = msg.value.decode('utf-8')
+    message2 = ast.literal_eval(message)
+
+    testing = []
     for i in cols:
         testing.append(float(message2[i]))
 
-      
-    test = pd.DataFrame ([testing], columns = cols)
-    test = pd.DataFrame(scaler.transform(test), columns = test.columns)
+    test = pd.DataFrame([testing], columns=cols)
+    test = pd.DataFrame(scaler.transform(test), columns=test.columns)
 
-    test_preds= grid_clf_acc.predict(test)
+    test_preds = grid_clf_acc.predict(test)
 
-    if test_preds[0]==0:
+    if test_preds[0] == 0:
         print("FLOW ID:", message2['FLOW_ID'], "result: BENIGN/REGULAR FLOW")
-    
-    if test_preds[0]==1:
+
+    if test_preds[0] == 1:
         print("FLOW ID:", message2['FLOW_ID'], "result: DDOS ATTACK FLOW")
-        ml_output="ddos"
-        counter = counter +1
-        output={"SOURCE":"ALGO112", "SEVERITY":"10", "DESCRIPTION":"DDoS LOIC", "DATA":{"SOURCE_IP":message2['IPV4_SRC_ADDR'], "SOURCE_PORT":message2['L4_SRC_PORT'], "DESTINATION_IP":message2['IPV4_DST_ADDR'],
-                "DESTINATION_PORT":message2['L4_DST_PORT'], "PROTOCOL":message2['PROTOCOL_MAP']}, "TIMESTAMP":time.time()}
+        ml_output = "ddos"
+        counter = counter + 1
+        output = {"SOURCE": "ALGO112", "SEVERITY": "10", "DESCRIPTION": "DDoS LOIC",
+                  "DATA": {"SOURCE_IP": message2['IPV4_SRC_ADDR'], "SOURCE_PORT": message2['L4_SRC_PORT'],
+                           "DESTINATION_IP": message2['IPV4_DST_ADDR'], "DESTINATION_PORT": message2['L4_DST_PORT'],
+                           "PROTOCOL": message2['PROTOCOL_MAP']}, "TIMESTAMP": time.time()}
         producer.send('detection-results', json.dumps(output).encode('utf-8'))
-
-        
-
--- version2/deployment.py
+++ version2/deployment.py
@@ -1,53 +1,60 @@
-from kafka import KafkaConsumer, KafkaProducer
-from joblib import dump, load
-import pandas as pd
+import ast
+import json
+import time
+from os import environ
+
 import numpy as np
-from os import listdir
-import sys, re, csv, ast, json, time
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-
-
+import pandas as pd
+from joblib import load
+from kafka import KafkaConsumer, KafkaProducer
 
 scaler = load('scaler.joblib')
 cols = load('columns.joblib')
 class_names = load('class_names.joblib')
-grid_clf_acc= load('rfmodel_multiclass.joblib')
+grid_clf_acc = load('rfmodel_multiclass.joblib')
 
-producer = KafkaProducer(bootstrap_servers='guard3.westeurope.cloudapp.azure.com:29092')
-consumer = KafkaConsumer('network-data', bootstrap_servers='guard3.westeurope.cloudapp.azure.com:29092')
+kafka_bootstrap_servers = environ.get(
+    'KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
+kafka_topic = environ.get('KAFKA_TOPIC', 'network-data')
 
+producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+consumer = KafkaConsumer(
+    kafka_topic, bootstrap_servers=kafka_bootstrap_servers)
 
 rep_time = 60
-time_to_report = time.time()+rep_time
-attackers={}
+time_to_report = time.time() + rep_time
+attackers = {}
 for msg in consumer:
-    
-    message = msg.value.decode('utf-8')
-    message2=ast.literal_eval(message)
 
-    testing=[]
+    message = msg.value.decode('utf-8')
+    message2 = ast.literal_eval(message)
+
+    testing = []
     for i in cols:
         testing.append(float(message2[i]))
-    #test = pd.DataFrame([testing], columns = cols)
-    test = pd.DataFrame(scaler.transform(np.asarray(testing).reshape(1, -1)), columns = cols)
-    test_preds= grid_clf_acc.predict(test)
-    print("FLOW ID:", message2['FLOW_ID'], "result: class", class_names[test_preds[0]])
+    # test = pd.DataFrame([testing], columns = cols)
+    test = pd.DataFrame(scaler.transform(
+        np.asarray(testing).reshape(1, -1)), columns=cols)
+    test_preds = grid_clf_acc.predict(test)
+    print("FLOW ID:", message2['FLOW_ID'],
+          "result: class", class_names[test_preds[0]])
 
-    if test_preds[0]!=0:
+    if test_preds[0] != 0:
         if message2["IPV4_SRC_ADDR"] not in attackers:
             attackers[message2["IPV4_SRC_ADDR"]] = {}
         if class_names[test_preds[0]] not in attackers[message2["IPV4_SRC_ADDR"]]:
-            attackers[message2["IPV4_SRC_ADDR"]][class_names[test_preds[0]]] = 1
+            attackers[message2["IPV4_SRC_ADDR"]
+                      ][class_names[test_preds[0]]] = 1
         else:
-            attackers[message2["IPV4_SRC_ADDR"]][class_names[test_preds[0]]] = attackers[message2["IPV4_SRC_ADDR"]][class_names[test_preds[0]]] + 1
+            attackers[message2["IPV4_SRC_ADDR"]][class_names[test_preds[0]]
+                                                 ] = attackers[message2["IPV4_SRC_ADDR"]][class_names[test_preds[0]]] + 1
 
     if time.time() >= time_to_report:
         if attackers:
-            output={"SOURCE":"ALGO112_RF", "SEVERITY":"10", "DESCRIPTION":"DDoS Attack(s)", "DATA":attackers, "TIMESTAMP":time.time()}
-            producer.send('detection-results', json.dumps(output).encode('utf-8'))
+            output = {"SOURCE": "ALGO112_RF", "SEVERITY": "10",
+                      "DESCRIPTION": "DDoS Attack(s)", "DATA": attackers, "TIMESTAMP": time.time()}
+            producer.send('detection-results',
+                          json.dumps(output).encode('utf-8'))
             print(attackers)
             attackers = {}
-        time_to_report = time.time()+rep_time
-
-
+        time_to_report = time.time() + rep_time
