37130d6868b072ea1f3d9b1c9a4a423fc6df1b78
Alex Carrega <contact@alexcarrega.com>
2021-08-02 11:12:35 +0200

    Simulate date update

--- version3/data/simulate_data.py
+++ version3/data/simulate_data.py
@@ -1,27 +1,30 @@
-from kafka import KafkaConsumer, KafkaProducer
-from joblib import dump, load
-import pandas as pd
-import numpy as np
-from os import listdir
-import sys, re, csv, ast, json, time
+import json
+import time
+from os import environ
 
+from kafka import KafkaProducer
 
-#new from Panagiotis
-file = open('vdpi_output_eth0_attack_22Jun21.log', 'r')
-#file = open('vdpi_output_eth0_normal_22Jun21.log', 'r')
-#old
-#file = open('vdpi_output_13Apr21.log', 'r')
-#file = open('vdpi_output_22Jun21_3.log', 'r')
+source_type = environ.get('SOURCE_TYPE', 'attack')
+kafka_bootstrap_servers = environ.get(
+    'KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
+kafka_topic = environ.get('KAFKA_TOPIC', 'network-data')
+
+print(f'KAFKA_BOOTSTRAP_SERVERS: {kafka_bootstrap_servers}')
+print(f'KAFKA_TOPIC: {kafka_topic}')
+print(f'SOURCE_TYPE: {source_type}')
+
+file = open(f'vdpi_output_eth0_{source_type}_22Jun21.log', 'r')
+# file = open('vdpi_output_eth0_normal_22Jun21.log', 'r')
+# old
+# file = open('vdpi_output_13Apr21.log', 'r')
+# file = open('vdpi_output_22Jun21_3.log', 'r')
 
 lines = file.read().splitlines()
 file.close()
 
-
-producer = KafkaProducer(bootstrap_servers='guard3.westeurope.cloudapp.azure.com:29092')
-counter=0
+producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+counter = 0
 for line in lines:
     print(line)
-    producer.send('network-data', json.dumps(json.loads(line)).encode('utf-8'))
+    producer.send(kafka_topic, json.dumps(json.loads(line)).encode('utf-8'))
     time.sleep(0.05)
-    
-
