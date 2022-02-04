74f5d9b6c0c8eded05d1e999189236fbb79cb509
Alex Carrega <contact@alexcarrega.com>
2022-02-04 19:10:16 +0100

    Fix error

--- version3/src/deployment.py
+++ version3/src/deployment.py
@@ -54,22 +54,27 @@ for msg in consumer:
     test = pd.DataFrame(scaler.transform(
         np.asarray(testing).reshape(1, -1)), columns=cols)
     test_preds = grid_clf_acc.predict(test)
-    print("FLOW ID:", message2['FLOW_ID'], "result: class", class_names[test_preds[0]])
+    print("FLOW ID:", message2['FLOW_ID'], "result: class",
+          class_names[test_preds[0]])
 
     if test_preds[0] != 0:
         if message2["IPV4_SRC_ADDR"] not in attackers:
             attackers[message2["IPV4_SRC_ADDR"]] = {}
-        if class_names[test_preds[0]] not in attackers[message2["IPV4_SRC_ADDR"]]:
+        if class_names[test_preds[0]] not in attackers[
+                message2["IPV4_SRC_ADDR"]]:
             attackers[message2["IPV4_SRC_ADDR"]
                       ][class_names[test_preds[0]]] = 1
         else:
-            attackers[message2["IPV4_SRC_ADDR"]]
-            [class_names[test_preds[0]]] = attackers[message2["IPV4_SRC_ADDR"]][class_names[test_preds[0]]] + 1
+            attackers[message2["IPV4_SRC_ADDR"]][
+                class_names[test_preds[0]]
+            ] = attackers[message2["IPV4_SRC_ADDR"]][
+                class_names[test_preds[0]]] + 1
 
     if time.time() >= time_to_report:
         if attackers:
             output = {"SOURCE": "ALGO112_v3", "SEVERITY": "10",
-                      "DESCRIPTION": "DDoS Attack(s)", "DATA": attackers, "TIMESTAMP": time.time()}
+                      "DESCRIPTION": "DDoS Attack(s)", "DATA": attackers,
+                      "TIMESTAMP": time.time()}
             producer.send('detection-results',
                           json.dumps(output).encode('utf-8'))
             print(attackers)
