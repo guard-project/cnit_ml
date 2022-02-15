9cca4728717724a85d2474aa07f5b202c17d670c
Alex Carrega <contact@alexcarrega.com>
2022-02-15 16:06:12 +0100

    Log output

--- version3/src/deployment.py
+++ version3/src/deployment.py
@@ -1,5 +1,6 @@
 import ast
 import json
+import logging
 import os
 import time
 import warnings
@@ -10,14 +11,25 @@ from about import project, title, version  # noqa: E402
 from dynaconf import Dynaconf
 from joblib import load
 from kafka import KafkaConsumer, KafkaProducer
-from rich import pretty, traceback  # noqa: E402
-from rich.console import Console  # noqa: E402
+from rich import pretty, print, traceback  # noqa: E402
+from rich.logging import RichHandler
 from rich.panel import Panel  # noqa: E402
 
 pretty.install()
 traceback.install(show_locals=False)
 
 config = Dynaconf(settings_files=["config.yaml"])
+
+log_data = config.get('log', {})
+log_level = log_data.get('level', 'NOTSET')
+log_format = log_data.get('format', '%(message)s')
+logging.basicConfig(
+    level=log_level, format=log_format,
+    datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=True,
+                                          omit_repeated_times=False,
+                                          markup=True)]
+)
+log = logging.getLogger("rich")
 
 pid = str(os.getpid())
 with open(".pidfile", "w") as f:
@@ -32,70 +44,71 @@ with warnings.catch_warnings():
     class_names = load("joblib/class_names.joblib")
     grid_clf_acc = load("joblib/rfmodel_multiclass_new.joblib")
 
-kafka_bootstrap_servers = config.kafka.bootstrap_servers
-if isinstance(kafka_bootstrap_servers, list):
-    kafka_bootstrap_servers = ",".join(kafka_bootstrap_servers)
-kafka_topic = config.kafka.topic
-kafka_group_id = config.kafka.group_id
+    kafka_bootstrap_servers = config.kafka.bootstrap_servers
+    if isinstance(kafka_bootstrap_servers, list):
+        kafka_bootstrap_servers = ",".join(kafka_bootstrap_servers)
+    kafka_topic = config.kafka.topic
+    kafka_group_id = config.kafka.group_id
 
-ident = f"{project} - {title} v:{version}"
+    ident = f"{project} - {title} v:{version}"
 
-console = Console()
-console.print("")
-console.print(Panel.fit(ident))
-console.print(f"Kafka Bootstrap Servers: {kafka_bootstrap_servers}")
-console.print(f"Kafka Topic: {kafka_topic}")
-console.print(f"Kafka Group ID: {kafka_group_id}")
+    print("")
+    print(Panel.fit(ident))
 
-producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
-consumer = KafkaConsumer(
-    kafka_topic,
-    group_id=kafka_group_id,
-    bootstrap_servers=kafka_bootstrap_servers,
-    auto_offset_reset="earliest",
-)
+    print(f"Kafka Bootstrap Servers: {kafka_bootstrap_servers}")
+    print(f"Kafka Topic: {kafka_topic}")
+    print(f"Kafka Group ID: {kafka_group_id}")
+    print(f"LOG Format: {log_format}")
+    print(f"LOG Level: {log_level}")
 
-rep_time = 60
-time_to_report = time.time() + rep_time
-attackers = {}
-for msg in consumer:
-    message = msg.value.decode("utf-8")
-    message2 = ast.literal_eval(message)
-
-    testing = [float(message2[i]) for i in cols]
-    # test = pd.DataFrame([testing], columns = cols)
-    test = pd.DataFrame(
-        scaler.transform(np.asarray(testing).reshape(1, -1)), columns=cols
+    producer = KafkaProducer(bootstrap_servers=kafka_bootstrap_servers)
+    consumer = KafkaConsumer(
+        kafka_topic,
+        group_id=kafka_group_id,
+        bootstrap_servers=kafka_bootstrap_servers,
+        auto_offset_reset="earliest",
     )
-    test_preds = grid_clf_acc.predict(test)
-    print("FLOW ID:", message2["FLOW_ID"], "result: class",
-          class_names[test_preds[0]])
 
-    if test_preds[0] != 0:
-        if message2["IPV4_SRC_ADDR"] not in attackers:
-            attackers[message2["IPV4_SRC_ADDR"]] = {}
-        if class_names[test_preds[0]] not \
-                in attackers[message2["IPV4_SRC_ADDR"]]:
-            attackers[message2["IPV4_SRC_ADDR"]][
-                class_names[test_preds[0]]] = 1
-        else:
-            attackers[message2["IPV4_SRC_ADDR"]][
-                class_names[test_preds[0]]] = (
-                attackers[message2["IPV4_SRC_ADDR"]][
-                    class_names[test_preds[0]]] + 1
-            )
+    rep_time = 60
+    time_to_report = time.time() + rep_time
+    attackers = {}
+    for msg in consumer:
+        message = msg.value.decode("utf-8")
+        message2 = ast.literal_eval(message)
 
-    if time.time() >= time_to_report:
-        if attackers:
-            output = {
-                "SOURCE": "ALGO112_v3",
-                "SEVERITY": "10",
-                "DESCRIPTION": "DDoS Attack(s)",
-                "DATA": attackers,
-                "TIMESTAMP": time.time(),
-            }
-            producer.send("detection-results",
-                          json.dumps(output).encode("utf-8"))
-            print(attackers)
-            attackers = {}
-        time_to_report = time.time() + rep_time
+        testing = [float(message2[i]) for i in cols]
+        # test = pd.DataFrame([testing], columns = cols)
+        test = pd.DataFrame(
+            scaler.transform(np.asarray(testing).reshape(1, -1)),
+            columns=cols
+        )
+        test_preds = grid_clf_acc.predict(test)
+        class_name_test_preds = class_names[test_preds[0]]
+        log.info(f'Flow ID: [red]{message2["FLOW_ID"]}[/red] - Result Class: [red]{class_name_test_preds}[/red]')
+
+        if test_preds[0] != 0:
+            ipv4_src_addr = message2["IPV4_SRC_ADDR"]
+            if ipv4_src_addr not in attackers:
+                attackers[ipv4_src_addr] = {}
+
+            class_name_test_preds = class_names[test_preds[0]]
+            if class_name_test_preds not in attackers[ipv4_src_addr]:
+                attackers[ipv4_src_addr][class_name_test_preds] = 1
+            else:
+                attackers[ipv4_src_addr][class_name_test_preds] = \
+                    attackers[ipv4_src_addr][class_name_test_preds] + 1
+
+        if time.time() >= time_to_report:
+            if attackers:
+                output = {
+                    "SOURCE": "ALGO112_v3",
+                    "SEVERITY": "10",
+                    "DESCRIPTION": "DDoS Attack(s)",
+                    "DATA": attackers,
+                    "TIMESTAMP": time.time(),
+                }
+                producer.send("detection-results",
+                              json.dumps(output).encode("utf-8"))
+                log.warning(f'Attackers: {attackers}')
+                attackers = {}
+            time_to_report = time.time() + rep_time
