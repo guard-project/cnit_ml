4caaea1045f4dd557f3d4300ad50fe8adfa52a2c
Alex Carrega <contact@alexcarrega.com>
2022-02-17 22:51:36 +0100

    flake8 fix

--- version3/src/training2.py
+++ version3/src/training2.py
@@ -17,7 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import (accuracy_score, balanced_accuracy_score, classification_report, confusion_matrix, f1_score,
-                             plot_confusion_matrix, precision_score, recall_score)
+                             precision_score, recall_score)
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
